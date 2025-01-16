d8e63af784cce65b9e25c0a8238c84e21be4ac52
alondranicc <aoliverasrobledo024@floridapoly.edu>
2025-01-16 16:54:15 +0000

    Created a program with user input

---
+++ 03_get_input/get_input.c
@@ -0,0 +1,26 @@
+/*
+getting the input from the keyboard.
+
+*/
+#include <stdio.h>
+int main() {
+    int square_side = 1;
+    int square_perimeter = 0;
+    int square_area = 0;
+//write the prompt
+puts("Enter the side of a square");
+
+//get the input
+//no messages in the quotes in scan f 
+//& is also called address operator
+scanf("%d", &square_side);
+
+// do the math 
+square_perimeter = square_side * 4;
+// create the output
+    printf("The perimeter of a square with side %d is %d\n",square_side, square_perimeter);
+
+
+return 0;
+
+}
